c168e3e4107dd487ac35cef80bf4f3f67caa973a
DESKTOP-VQL3FIK\sp <showpang2013@gmail.com>
2020-05-27 17:45:11 +0900

    세번째 커밋

--- src/main/java/com/spsp/inflearn/intellj/Main.java
+++ src/main/java/com/spsp/inflearn/intellj/Main.java
@@ -18,6 +18,12 @@ public class Main {
         System.out.println("10");
         System.out.println("10");
         System.out.println("10");
+        System.out.println("10");
+        System.out.println("10");
+        System.out.println("10");
+        System.out.println("10");
+        System.out.println("10");
+        System.out.println("10");
 
     }
 
